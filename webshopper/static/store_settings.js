e78447f7bbf8c68a12c9155c5e0457f8e08c1bb9
Ryan <ryan.eliopoulos@gmail.com>
2022-03-11 13:26:24 -0800

    settings

--- webshopper/static/store_settings.js
+++ webshopper/static/store_settings.js
@@ -11,8 +11,10 @@ function get_locations() {
     let zipcode = document.getElementById('zipbox_input').value;
     console.log(` Here is the host name${window.location.protocol}`);
     console.log(` Here is the host name${window.location.hostname}`);
-    
-    let url = new URL(sessionStorage.getItem('get_location_url'), );
+    let url_string = window.location.protocol + '//' + window.location.hostname + ':5000'
+//    let url = new URL(`${window.location.protocsessionStorage.getItem('get_location_url'), );
+    let url = sessionStorage.getItem('get_location_url');
+    console.log(`full fancy url? ${url}`);
     url.set('zipcode', zipcode);
     // Initializing request and attaching callback function
     let response = fetch(url, {
