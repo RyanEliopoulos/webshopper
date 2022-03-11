9b98d6594cf529aabce5016286721f6ebd16fa6f
Ryan <ryan.eliopoulos@gmail.com>
2022-03-11 12:38:05 -0800

    ettingsa

--- webshopper/static/store_settings.js
+++ webshopper/static/store_settings.js
@@ -2,18 +2,22 @@
 function submit_listener() {
     // Should be able to retrieve the
     let sub_btn = document.getElementById("submit_button");
-    sub_btn.addEventListener("Mouse", (event) => {
-        // Input validation would go here
-        let zipcode = document.getElementById("zipbox_input").value;
-        console.log(`Here is the zipbox value: ${zipcode}`);
-    });
+    sub_btn.onclick = get_locations();
+//    sub_btn.addEventListener("Mouse", (event) => {
+//        // Input validation would go here
+//        let zipcode = document.getElementById("zipbox_input").value;
+//        console.log(`Here is the zipbox value: ${zipcode}`);
+//    });
 
     let url = sessionStorage.getItem('get_location_url');
     console.log(`Also checking the storage thing: ${url}`);
 }
 
 
-function get_locations(get_endpoint) {
+function get_locations() {
+    console.log('in get_locations');
+    let zipcode = document.getElementById('zipbox_input').value;
+    console.log(`Here is the zipbox value: ${zipcode}`);
 }
 
 function set_location(set_endpoint) {
