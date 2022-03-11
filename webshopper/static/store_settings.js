73775d4498c8d08411d7bcf5e39200c1275171d6
Ryan <ryan.eliopoulos@gmail.com>
2022-03-11 12:34:53 -0800

    settings

--- webshopper/static/store_settings.js
+++ webshopper/static/store_settings.js
@@ -1,7 +1,15 @@
 
-function submit_listener(get_endpoint) {
-    console.log("Testing the listener prototype");
-    console.log(`${get_endpoint}`);
+function submit_listener() {
+    // Should be able to retrieve the
+    let sub_btn = document.getElementById("submit_button");
+    sub_btn.addEventListener("Mouse", (event) => {
+        // Input validation would go here
+        let zipcode = document.getElementById("zipbox_input").value;
+        console.log(`Here is the zipbox value: ${zipcode}`);
+    });
+
+    let url = sessionStorage.getItem('get_location_url');
+    console.log(`Also checking the storage thing: ${url}`);
 }
 
 
