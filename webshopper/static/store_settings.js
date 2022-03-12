493d2bc88dc28a9dd86232686584d38e73a59c78
Ryan <ryan.eliopoulos@gmail.com>
2022-03-12 15:45:40 -0800

    settings

--- webshopper/static/store_settings.js
+++ webshopper/static/store_settings.js
@@ -19,12 +19,47 @@ function get_locations() {
     })
     .then (response => response.json())  // Deserializing the json response
     .then  ( data => { // Working with the json object
+            // @TODO response parsing here? Not sure
           console.log(data.results);
-//        console.log(`Received response ${response.status} ${response.statusText}`);
-//        console.log(`${response.body}`);
-//        console.log(`${response.json()}`);
+          update_ui_locations(data);
+          // So we have the store locations. Remove old html elements
+          // and replace with a list of locations to choose from
+
     });
 }
+
+
+function update_ui_locations(locations) {
+    // Called by get_locations after receiving location data from our API
+    // locations = list of JSON objects
+    // keys 'address', 'locationId', 'chain'
+
+    // Remove existing buttons/elements
+    list_cleanup();
+    // update with a list of elements..how do we record selection?
+    let main_div = document.getElementById('main_div');  // To attach the new list divs onto
+    locations.foreach(element => {
+        // Updating new div with store details
+        let tmp_div = document.createElement("div");
+        let chain_node = document.createTextNode(element.chain);
+        tmp_div.appendChild(chain_node);
+        let br = document.createElement("br");
+        tmp_div.appendChild(br);
+        let address_node = document.createTextNode(element.address);
+        tmp_div.appendChild(address_node);
+        // Adding this div to the main div
+        main_div.appendChild(tmp_div);
+    });
+}
+
+function list_cleanup() {
+    // Finds and deletes all list item containers
+    let elements = document.getElementsByClassName('location_item');
+    elements.foreach(element => {
+        element.remove();
+    });
+}
+
 
 function set_location(set_endpoint) {
 }
