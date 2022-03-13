fe48f79c8347bd77f09800df5112a3c90e294deb
Ryan <ryan.eliopoulos@gmail.com>
2022-03-12 16:04:28 -0800

    settings

--- webshopper/static/store_settings.js
+++ webshopper/static/store_settings.js
@@ -55,6 +55,7 @@ function update_ui_locations(locations) {
 function list_cleanup() {
     // Finds and deletes all list item containers
     let elements = document.getElementsByClassName('location_item');
+    console.log(elements.length);
     if (typeof(elements) === 'undefined') {
         console.log('Yep, it\'s undefined');
         return;
